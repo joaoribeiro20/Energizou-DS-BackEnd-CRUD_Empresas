c1fd1e8b75e3595e5b1b2bf0cc5b8e83729582d8
unknown <jvalves217@gmail.com>
2023-10-30 22:06:13 -0300

    -add cors

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import express from 'express'
 import { AppDataSource } from './data-source'
 import { loadavg } from 'os'
 import routes from './routes'
+const cors = require('cors');
 //import routes from './routes'
 
 
 AppDataSource.initialize().then(() => {
 	const app = express()
-
+	app.use(cors())
 	app.use(express.json())
 
 	app.use(routes)
